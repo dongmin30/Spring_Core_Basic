8e16842056375762850418e70301f8ded435dd1a
Dongmin_Kim <66549954+dongmin30@users.noreply.github.com>
2023-01-05 23:40:11 +0900

    스프링 핵심 원리 이해2 - AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,7 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +10,22 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    //-- 역할과 구현의 분리를 위해 리펙토링 작업 - 역할
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    //-- 역할과 구현의 분리를 위해 리펙토링 작업 - 구현체
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
